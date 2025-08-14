be2c0f6f2150468ae346326d69097cb21341cbfa
WebPr0x <allthefactsemail@gmail.com>
2025-08-13 20:07:40 -0400

    Update server.js

--- server.js
+++ server.js
@@ -10,16 +10,29 @@ const io = new Server(server, {
   }
 });
 
+let messageHistory = []; // Store last 20 messages
+
 io.on("connection", (socket) => {
   console.log("User connected: " + socket.id);
 
+  // Send last 20 messages to new client
+  socket.emit("chat history", messageHistory);
+
   socket.on("chat message", (msg) => {
     if (msg && msg.username && msg.message) {
-      io.emit("chat message", {
+      const data = {
         id: socket.id.slice(0, 5),
         username: msg.username,
         message: msg.message
-      });
+      };
+
+      // Add to history
+      messageHistory.push(data);
+      if (messageHistory.length > 20) {
+        messageHistory.shift();
+      }
+
+      io.emit("chat message", data); // Broadcast to all clients
     }
   });
 
